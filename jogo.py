3d02b5cb481f8869610d0ec6045a107e5248abac
Rafael Scariot <rafael.scariot_bols@compasso.com.br>
2019-06-26 16:56:39 -0300

    atividade 03: construindo narrativas e implementando discionários

--- jogo.py
+++ jogo.py
@@ -1,4 +1,25 @@
-import os
+jogador = {
+    'amigos': [],
+    'inimigos': [],
+    'caminhos': [],
+    'vida': 100,
+    'arma': 'soco'
+            }
+ataques = {
+    'fraco' : 20,
+    'medio' : 50,
+    'forte' : 70,
+    'especial' : 100
+}
+
+def carregar_arquivo_personagem ():
+    arquivo = open('personagens.txt', 'r')
+    personagens = arquivo.read().split("\n")
+    arquivo.close()
+
+    return personagens
+
+personagens = carregar_arquivo_personagem()
 
 def start ():
     sair = 0
@@ -9,22 +30,15 @@ def start ():
 
         if (opcao == 1):
             nome_jogador = input("Informe seu nome: ").strip().upper()
-            carregar_arquivo_personagem()
             welcome(nome_jogador)
         elif (opcao == 2):
             sair = 1
         else:
             print("Opção inválida!")
 
-def carregar_arquivo_personagem ():
-    arquivo = open('personagens.txt', 'r')
-    personagens = arquivo.read().split("\n")
-    arquivo.close()
 
-    return personagens
 
 def welcome (nome_jogador):
-    #print('\033[32m''\033[0;0m')
     print("*"*58)
     print("")
     print("\t\t\tBEM-VINDO {}, JOVEM GUERREIRO".format(nome_jogador).upper())
@@ -33,9 +47,14 @@ def welcome (nome_jogador):
     print("")
     print("*"*58)
 
-    print("Após a queda da Igreja na capital, o caos foi se espalhando aos poucos e agora cabe a você salvar o Reino.\nVocê deve fazer suas escolhas com honra e sabedoria, pois elas afetaram diretamente a sua jornada. Que a força esteja com você...")
+    print("Após a queda da Igreja na capital, o caos foi se espalhando, e o mundo foi dominado pelo {}, aos poucos e agora cabe a você salvar o Reino.\nVocê deve fazer suas escolhas com honra e sabedoria, pois elas afetaram diretamente a sua jornada. Que a força esteja com você...".format(personagens[0]))
 
-    print("\nPara salvar o Reino, você deve chegar a capital para batalhar, entretanto, até lá você terá uma árdua jornada.")
+
+    jogador['caminhos'].append(escolha_caminho())
+
+def escolha_caminho():
+    print(
+        "\nPara salvar o Reino, você deve chegar a capital para batalhar, entretanto, até lá você terá uma árdua jornada.")
     print("Com cuidado, decida qual caminho você irá tomar: ")
     print("[1] - Ir pelas masmorras")
     print("[2] - Seguir o rio")
@@ -43,16 +62,17 @@ def welcome (nome_jogador):
     while (True):
         opcao_cenario = int(input("\nDigite sua decisão: "))
         if (opcao_cenario == 1):
-            masmorras()
+            jogador['amigos'].append(masmorras())
+            return "masmorras"
         elif (opcao_cenario == 2):
             seguir_o_rio()
+            return "rio"
         else:
             print("Decisão inválida!")
 
 def masmorras():
-    personagem = carregar_arquivo_personagem()
-    print("\nAo se aproximar das masmorras, você avista um velho com armadura feita de pedra, ele é o {}.".format(personagem[0]))
-    print("O {} se aproxima de você e vocês começam a conversar. Ele houve a sua história e se impressiona com a sua \nintenção de salvar o reino e decide lhe oferecer ajuda. Você aceita?".format(personagem[0]))
+    print("\nAo se aproximar das masmorras, você avista um velho com armadura feita de pedra, ele é o {}.".format(personagens[1]))
+    print("O {} se aproxima de você e vocês começam a conversar. Ele houve a sua história e se impressiona com a sua \nintenção de salvar o reino e decide lhe oferecer ajuda. Você aceita?".format(personagens[1]))
     print("[1] - Aceitar ajuda")
     print("[2] - Negar ajuda")
 
@@ -60,6 +80,7 @@ def masmorras():
         opcao_decisao = int(input("\nDigite sua decisão: "))
         if (opcao_decisao == 1):
             aceitou_ajuda()
+            return personagens[1]
         elif (opcao_decisao == 2):
             negou_ajuda()
         else:
@@ -71,14 +92,26 @@ def seguir_o_rio():
     print("[1] - Ir conversar com ela")
     print("[2] - Ignora-lá e seguir com sua jornada, afinal, é isso que importa para você")
 
+    while (True):
+        opcao_decisao = int(input("\nDigite sua decisão: "))
+        if (opcao_decisao == 1):
+            conversar_mulher()
+        elif (opcao_decisao == 2):
+            ignorar_a_mulher()
+        else:
+            print("Decisão inválida!")
+
 def aceitou_ajuda():
-    print("negou")
+    print("Ótima decisão! Agora, o {} é seu amigo e lhe forneceu uma de suas melhores armaduras.".format(personagens[0]))
+    print("Você obteve 20 pontos incrementados na sua vida, entretanto, o {} irá lhe acompanhar em sua jornada até a capital e irá atrasar sua viajem.".format(personagens[1]))
+    jogador['vida'] += 20
 
 def negou_ajuda():
-    print("negou")
+    print("Você negou ajuda, sua jornada continuará, entretanto, o {} não lhe ajudará, e, você deixou de ganhar recompensas.".format(personagens[0]))
 
 def conversar_mulher():
-    print("conversar")
+    print("A mulher revelou ser a {}, fiel ajudante do {}, responsável pela falência da igreja e causador de todo o mal que o Reino sofre.".format(personagens[2], personagens[0]))
+    print("A {} lhe menosprezou dizendo que você é fraco e por isso não irá lhe enfrentar agora. Ela mandou você se fortalecer e jurou matar-lhe futuramente.".format(personagens[2]))
 
 def ignorar_a_mulher():
     print("ignorou")
