ea815c670a51163d1691791df5573167b123e384
Rafael Scariot <rafael.scariot_bols@compasso.com.br>
2019-06-26 13:49:31 -0300

    atividade 03: construindo mensagem de boas vindas

--- jogo.py
+++ jogo.py
@@ -7,18 +7,28 @@ def start ():
 
         if (opcao == 1):
             nome_jogador = input("Informe seu nome: ")
-            carregar_arquivos()
+            #carregar_arquivo_personagem()
+            welcome()
         elif (opcao == 2):
             sair = 1
         else:
             print("Opção inválida!")
 
-def carregar_arquivos ():
+def carregar_arquivo_personagem ():
     arquivo = open('personagens.txt', 'r')
     personagens = arquivo.read().split("\n")
     arquivo.close()
 
     return personagens
+
+def welcome ():
+    print("*"*50)
+    print("*                                                *")
+    print("\t\tBEM-VINDO AO MUNDO DO REI MALVADO            *")
+    print("\t\tDEFENDA SUA HONRA EM BATALHAS MORTAIS        *")
+    print("*\tSUAS ESCOLHAS DEFINIRAM O DESTINO DE SUA JORNADA*")
+    print("*                                                *")
+    print("*"*50)
 
 if (__name__ == "__main__"):
     start()
