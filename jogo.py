c68e02daf019318704b58a193a032089615696c0
Rafael Scariot <rafael.scariot_bols@compasso.com.br>
2019-06-26 15:25:44 -0300

    atividade 03: construção das escolhas primárias

--- jogo.py
+++ jogo.py
@@ -1,3 +1,5 @@
+import os
+
 def start ():
     sair = 0
     while (sair == 0):
@@ -8,8 +10,6 @@ def start ():
         if (opcao == 1):
             nome_jogador = input("Informe seu nome: ").strip().upper()
             carregar_arquivo_personagem()
-
-            #carregar_arquivo_personagem()
             welcome(nome_jogador)
         elif (opcao == 2):
             sair = 1
@@ -24,17 +24,64 @@ def carregar_arquivo_personagem ():
     return personagens
 
 def welcome (nome_jogador):
-
+    #print('\033[32m''\033[0;0m')
     print("*"*58)
     print("")
     print("\t\t\tBEM-VINDO {}, JOVEM GUERREIRO".format(nome_jogador).upper())
     print("\t\t  DEFENDA SUA HONRA EM BATALHAS MORTAIS")
-    print("\t  SUAS ESCOLHAS DEFINIRAM O DESTINO DE SUA JORNADA")
+    print("\t  SUAS ESCOLHAS DEFINIRÃO O DESTINO DE SUA JORNADA")
     print("")
     print("*"*58)
 
-    print("Após a queda da Igreja na capital, o caos foi se espalhando aos poucos e agora cabe a você salvar o Reino.\nVocê deve fazer suas escolhas com honra e sabedoria, pois elas afetaram diretamente a sua jornada. Que a força esteja com você.")
+    print("Após a queda da Igreja na capital, o caos foi se espalhando aos poucos e agora cabe a você salvar o Reino.\nVocê deve fazer suas escolhas com honra e sabedoria, pois elas afetaram diretamente a sua jornada. Que a força esteja com você...")
 
+    print("\nPara salvar o Reino, você deve chegar a capital para batalhar, entretanto, até lá você terá uma árdua jornada.")
+    print("Com cuidado, decida qual caminho você irá tomar: ")
+    print("[1] - Ir pelas masmorras")
+    print("[2] - Seguir o rio")
+
+    while (True):
+        opcao_cenario = int(input("\nDigite sua decisão: "))
+        if (opcao_cenario == 1):
+            masmorras()
+        elif (opcao_cenario == 2):
+            seguir_o_rio()
+        else:
+            print("Decisão inválida!")
+
+def masmorras():
+    personagem = carregar_arquivo_personagem()
+    print("\nAo se aproximar das masmorras, você avista um velho com armadura feita de pedra, ele é o {}.".format(personagem[0]))
+    print("O {} se aproxima de você e vocês começam a conversar. Ele houve a sua história e se impressiona com a sua \nintenção de salvar o reino e decide lhe oferecer ajuda. Você aceita?".format(personagem[0]))
+    print("[1] - Aceitar ajuda")
+    print("[2] - Negar ajuda")
+
+    while (True):
+        opcao_decisao = int(input("\nDigite sua decisão: "))
+        if (opcao_decisao == 1):
+            aceitou_ajuda()
+        elif (opcao_decisao == 2):
+            negou_ajuda()
+        else:
+            print("Decisão inválida!")
+
+def seguir_o_rio():
+    print("\nÉ noite quando você chega as margens do Rio das Máguas, seguindo o rio, em direção a capital, você avista uma esbelta mulher admirando a paisagem.")
+    print("Tomado por admiração com a beleza da mulher, você decide:")
+    print("[1] - Ir conversar com ela")
+    print("[2] - Ignora-lá e seguir com sua jornada, afinal, é isso que importa para você")
+
+def aceitou_ajuda():
+    print("negou")
+
+def negou_ajuda():
+    print("negou")
+
+def conversar_mulher():
+    print("conversar")
+
+def ignorar_a_mulher():
+    print("ignorou")
 
 if (__name__ == "__main__"):
     start()
