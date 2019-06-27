4173a245ee04a1f20a45be535bb93adfff04795c
Rafael Scariot <rafael.scariot_bols@compasso.com.br>
2019-06-27 12:56:54 -0300

    atividade 03: adicionando escolhas a narrativa

--- jogo.py
+++ jogo.py
@@ -3,8 +3,9 @@ jogador = {
     'inimigos': [],
     'caminhos': [],
     'vida': 100,
+    'sabedoria': 100,
     'arma': 'soco'
-            }
+}
 ataques = {
     'fraco' : 20,
     'medio' : 50,
@@ -36,25 +37,21 @@ def start ():
         else:
             print("Opção inválida!")
 
-
-
 def welcome (nome_jogador):
     print("*"*58)
     print("")
-    print("\t\t\tBEM-VINDO {}, JOVEM GUERREIRO".format(nome_jogador).upper())
+    print("\t\t\tBEM-VINDO {}, JOVEM GUERREIRO(A)".format(nome_jogador).upper())
     print("\t\t  DEFENDA SUA HONRA EM BATALHAS MORTAIS")
     print("\t  SUAS ESCOLHAS DEFINIRÃO O DESTINO DE SUA JORNADA")
     print("")
     print("*"*58)
 
-    print("Após a queda da Igreja na capital, o caos foi se espalhando, e o mundo foi dominado pelo {}, aos poucos e agora cabe a você salvar o Reino.\nVocê deve fazer suas escolhas com honra e sabedoria, pois elas afetaram diretamente a sua jornada. Que a força esteja com você...".format(personagens[0]))
-
+    print("Após a queda da Igreja na capital, o caos foi se espalhando, e o mundo foi dominado pelo {}, e, agora cabe a você salvar o Reino.\nVocê deve fazer suas escolhas com honra e sabedoria, pois elas afetarão diretamente a sua jornada. Que a força esteja com você...".format(personagens[0]))
 
     jogador['caminhos'].append(escolha_caminho())
 
 def escolha_caminho():
-    print(
-        "\nPara salvar o Reino, você deve chegar a capital para batalhar, entretanto, até lá você terá uma árdua jornada.")
+    print("\nPara salvar o Reino, você deve chegar a capital para batalhar, entretanto, até lá você terá uma árdua jornada.")
     print("Com cuidado, decida qual caminho você irá tomar: ")
     print("[1] - Ir pelas masmorras")
     print("[2] - Seguir o rio")
@@ -102,19 +99,22 @@ def seguir_o_rio():
             print("Decisão inválida!")
 
 def aceitou_ajuda():
-    print("Ótima decisão! Agora, o {} é seu amigo e lhe forneceu uma de suas melhores armaduras.".format(personagens[0]))
-    print("Você obteve 20 pontos incrementados na sua vida, entretanto, o {} irá lhe acompanhar em sua jornada até a capital e irá atrasar sua viajem.".format(personagens[1]))
+    print("Ótima decisão! Agora, o {} é seu amigo e lhe forneceu uma de suas melhores armaduras.".format(personagens[1]))
+    print("Você obteve 20 pontos de vida, entretanto, o {} irá lhe acompanhar em sua jornada até a capital e irá atrasar sua viajem.".format(personagens[1]))
     jogador['vida'] += 20
 
 def negou_ajuda():
-    print("Você negou ajuda, sua jornada continuará, entretanto, o {} não lhe ajudará, e, você deixou de ganhar recompensas.".format(personagens[0]))
+    print("Você negou ajuda, sua jornada continuará, entretanto, o {} não lhe ajudará, e, você deixou de ganhar recompensas.".format(personagens[1]))
 
 def conversar_mulher():
     print("A mulher revelou ser a {}, fiel ajudante do {}, responsável pela falência da igreja e causador de todo o mal que o Reino sofre.".format(personagens[2], personagens[0]))
     print("A {} lhe menosprezou dizendo que você é fraco e por isso não irá lhe enfrentar agora. Ela mandou você se fortalecer e jurou matar-lhe futuramente.".format(personagens[2]))
 
 def ignorar_a_mulher():
-    print("ignorou")
+    print("Ignorar a mulher foi uma sábia decisão, pois ela era a {}, fiel ajudante do {}. O qual você precisa enfrentar para acabar com a escuridão que assombra o reino.".format(personagens[2], personagens[0]))
+    print("A {} tentará impedir-lhe de matar o {} a todo custo. Siga sua jornada com cuidado...".format(personagens[2], personagens[0]))
+    print("Você ganhou 20 pontos de sabedoria.")
+    jogador['sabedoria'] += 20
 
 if (__name__ == "__main__"):
     start()
